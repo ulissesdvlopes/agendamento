f226e0586792cf28827af8730985b882a7ffdcc0
Ulisses Lopes <ulopes@sumaconnect.com>
2024-02-10 02:58:39 -0300

    Configuring cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(8000);
